5f7b9829c2517c0e80db70f31fe05f82c49b73d1
aexto9 <mushab0911@gmail.com>
2023-12-14 18:50:49 +0700

    Pertemuan-11

---
+++ SetTimeout/set-timeout-2.js
@@ -0,0 +1,8 @@
+console.log("Abhe Membuka Chrome");
+
+setTimeout(() => {
+    console.log("Mendownload file");
+    console.log("Download selesai");
+}, 5000);
+
+console.log("Abhe Membuka Twitter");
